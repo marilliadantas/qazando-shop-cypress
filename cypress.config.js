2142ad97e341f1ec37a3625fda7c7135259f8231
Marillia Dantas <marillia.dantas@hotmail.com>
2024-02-18 17:42:51 -0300

    ajuste de ci

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     "viewportWidth": 1920,
     "viewportHeight": 1080,
     "video": false,
-    "screenshotOnRunFailure": false,
+    // "screenshotOnRunFailure": false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
