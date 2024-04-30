3dcef80adc3f8ecef41fe4450841716bb7a102bd
Marillia Dantas <marillia.dantas@hotmail.com>
2024-04-29 22:50:15 -0300

    implementando o report

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 // import './commands'
-import '@shelex/cypress-allure-plugin';
+require('@shelex/cypress-allure-plugin');
 
 const app = window.top;
 if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
